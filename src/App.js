84ff220469dacb9bddfce66fce07b3d2f9f067be
ViktorDojchinovski <viktorjano@yahoo.com>
2024-01-31 23:16:10 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// JSX
+// <div>Hello World!</div> --> React.createElement('<div>Hello World!</div>');
+// --> createElement() --> React.createHtmlElement('div') --> document.createElement('div');
+
+/**
+ * <body>
+ *  <button onclick=()>Click</button>
+ * </body>
+ * <script>
+ *  const tag = 'paragraph';
+ *  const sentence = `Some basic ${tag} tag.`
+ *
+ *  document.getElementById().text(sentence);
+ * </script>
+ */
+function Content() {
+  const tag = "paragraph";
+  const sentence = `Some basic ${tag} tag.`;
+  return (
+    <>
+      <div>
+        <a href="https://google.com">Google</a>
+        <p onClick>{sentence}</p>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="first">Hello World!</div>
+      <div className="second">Something!</div>
+      <Content></Content>
+    </>
   );
 }
 
